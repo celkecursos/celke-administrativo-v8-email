8acb06d4957988c469dc696b681e3d49072ebf98
Cesar Szpak <cesar@celke.com.br>
2025-12-04 10:41:28 -0300

    Criar a models para a tabela email_tags

---
+++ app/Models/EmailTag.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class EmailTag extends Model implements Auditable
+{
+    use \OwenIt\Auditing\Auditable;
+    // Indicar o nome da tabela
+    protected $table = 'email_tags';
+
+    // Indicar quais colunas podem ser manipuladas
+    protected $fillable = [
+        'name',
+        'is_active',
+    ];
+
+    /**
+     * Casts de atributos.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+}
